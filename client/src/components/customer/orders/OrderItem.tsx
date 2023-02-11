74e717ae1f5e8ff39cfadb822912df5d841b596b
Sreeya Meela <sreeyameela@Sreeyas-Air.lan>
2023-02-11 19:00:00 +0530

    refractoring changes

--- client/src/components/customer/orders/OrderItem.tsx
+++ client/src/components/customer/orders/OrderItem.tsx
@@ -1,11 +1,39 @@
-import React from 'react'
 
-function OrderItem() {
-  return (
-    <div>
-      Order ITem
-    </div>
-  )
+export interface OrderInfo {
+  id: string;
+  productName: string;
+  category: string;
+  price: number;
+  quantity: number;
+  url: string;
 }
 
-export default OrderItem
+  return (
+    <Card maxW={"72"} key={id}>
+      {!isAvailable && (
+        <CornerRibbon
+        >
+          NOT AVAILABLE
+        </CornerRibbon>
+      )}
+      <CardBody>
+        <Image
+        />
+        <Stack mt="6" spacing="3">
+          <Heading size="md">{productName}</Heading>
+          <Text>{description}</Text>
+        </Stack>
+      </CardBody>
+      <CardFooter justifyContent="space-between" alignItems={"center"}>
+        <Text fontSize={"lg"} fontWeight="semibold">
+          ${price}
+        </Text>
+        <Button
+        >
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
+
+export default OrderItem;
