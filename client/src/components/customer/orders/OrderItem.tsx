37f87e9e6dcaa99ae4d66ab959d8980d4506501b
Sreeya Meela <sreeyameela@Sreeyas-Air.lan>
2023-02-11 19:30:00 +0530

    order item view

--- client/src/components/customer/orders/OrderItem.tsx
+++ client/src/components/customer/orders/OrderItem.tsx
@@ -1,6 +1,18 @@
-
+import {
+  Avatar,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  Divider,
+  Flex,
+  Heading,
+  Stack,
+  Text,
+  Image,
+} from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
-
 interface OrderItemsProps {
   id: string;
   productName: string;
@@ -53,19 +65,32 @@ const OrderItem = (props: OrderItemsProps) => {
 
     fetchImage();
   }, []);
-  // const { cartData, setCartData } = useCart();
 
   };
   return (
     <Card maxW={"72"} key={id}>
       {!isAvailable && (
         <CornerRibbon
+          position="top-right" // OPTIONAL, default as "top-right"
+          fontColor="#fff" // OPTIONAL, default as "#f0f0f0"
+          backgroundColor="#FF0000" // OPTIONAL, default as "#2c7"
+          containerStyle={{}} // OPTIONAL, style of the ribbon
+          style={{}} // OPTIONAL, style of ribbon content
+          className="" // OPTIONAL, css class of ribbon
         >
           NOT AVAILABLE
         </CornerRibbon>
       )}
       <CardBody>
         <Image
+          filter="auto"
+          brightness="70%"
+          maxHeight={"56"}
+          maxW={"100%"}
+          minW="100%"
+          src={url}
+          alt="Green double couch with wooden legs"
+          borderRadius="lg"
         />
         <Stack mt="6" spacing="3">
           <Heading size="md">{productName}</Heading>
@@ -77,6 +102,10 @@ const OrderItem = (props: OrderItemsProps) => {
           ${price}
         </Text>
         <Button
+          variant={"solid"}
+          colorScheme={isAvailable ? "orange" : "gray"}
+          disabled={!isAvailable}
+          rounded={"full"}
           onClick={onItemClicked}
         >
           Add to cart
