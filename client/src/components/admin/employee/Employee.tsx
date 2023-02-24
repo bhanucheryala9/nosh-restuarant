40f48f22dae62fb889d4d8a1b8f9f8cb6995c5f1
Sreeya Meela <sreeyameela@Sreeyas-Air.lan>
2023-02-24 05:30:00 -0400

    employee list grid display

--- client/src/components/admin/employee/Employee.tsx
+++ client/src/components/admin/employee/Employee.tsx
@@ -3,94 +3,172 @@ import { Input, InputRef, Space, Table, Tag } from "antd";
 import {
   Flex,
   Grid,
+  GridItem,
+  Text,
+  Avatar,
+  HStack,
+  Link,
+  Code,
+  VStack,
+  Button,
+  Icon,
+  IconButton,
 } from "@chakra-ui/react";
-import AddEmployee from "./AddEmployee";
-import { useNotification } from "../../../contexts/Notification";
-import { useNavigate } from "react-router-dom";
+import { DeleteIcon, EditIcon, EmailIcon, PhoneIcon } from "@chakra-ui/icons";
+import { faker } from "@faker-js/faker";
 import _ from "lodash";
 
-export interface EmployeeDatatype {
-  key: React.Key;
-  id: string;
-  name: string;
-  email: string;
-  phoneNumber: string;
-  employeeType: string;
-  address: string;
-  salary: string;
-  joinedDate: string;
-  about?: string;
-}
+
 const Employee = () => {
   const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
   const [addEmployeeModal, setAddEmployeeModal] = useState<boolean>(false);
   const [unformattedEmployeeData, setunformattedEmployeeData] = useState([]);
   const [employeeData, setEmployeeData] = useState<Array<EmployeeDatatype>>([]);
-  const [forUpdate, setForUpdate] = useState<boolean>(false);
-  const [userProfile, setUserProfile] = useState<EmployeeDatatype>(
     employeeData[0]
   );
-
   type DataIndex = keyof EmployeeDatatype;
 
   const handleSearch = (
-    selectedKeys: string[],
-    confirm: (param?: FilterConfirmProps) => void,
-    dataIndex: DataIndex
-  ) => {
-    confirm();
-    setSearchText(selectedKeys[0]);
-    setSearchedColumn(dataIndex);
   };
 
   return (
     <React.Fragment>
-      <Flex>
+      <Flex mx={{ base: "4", lg: "10" }} my="6" direction={"column"}>
+        <Text fontSize={"xl"} fontWeight="bold">
           Employees List
         </Text>
-        <Grid>
+        <Grid
+          mt="4"
+          templateRows={{ base: "repeat(2, 1fr)", lg: "repeat(1, 1fr)" }}
+          templateColumns={{ base: "repeat(1, 1fr)", lg: "repeat(6, 1fr)" }}
+          gap={{ base: 2, lg: 4 }}
+        >
           <GridItem colSpan={{ base: 1, lg: 4 }}>
-            <Flex>
+            <Flex
+              bg="white"
+              p="6"
+              borderRadius={"md"}
+              w="100%"
+              direction={"column"}
+            >
               <Flex justifyContent={"end"} mb="4">
-        
-                <Button>
+                {/* <InputGroup maxW="44" alignItems={"center"} rounded="md">
+                  <InputLeftElement
+                    pointerEvents="none"
+                    children={<SearchIcon color="gray.300" />}
+                  />
+                  <Input
+                    variant="filled"
+                    placeholder="Search.."
+                    size={{ base: "sm" }}
+                  />
+                </InputGroup> */}
+                <Button
+                  size={{ base: "sm", lg: "md" }}
+                  colorScheme="orange"
+                  onClick={() => setAddEmployeeModal(true)}
+                >
                   Add Employee
                 </Button>
               </Flex>
-           
+              <Table
+                onRow={(record, rowIndex) => {
+                  return {
+                    onClick: (event) => {
+                      setUserProfile(record);
+                    },
+                  };
+                }}
+                scroll={{ x: 400 }}
+                style={{ width: "100%" }}
+                size="large"
+                // rowSelection={rowSelection as any}
+                columns={columns}
+                dataSource={employeeData}
+              />
             </Flex>
           </GridItem>
           <GridItem colSpan={2} bg="white" borderRadius={"md"}>
-         
-              
+            <Flex p="4">
+              <Avatar
+                size="xl"
+                name={userProfile?.name}
+               
+              />
+              <Flex mx="4" direction={"column"}>
+                <Text
+                  fontSize={"xl"}
+                  fontFamily="semibold"
+                  textColor={"orange.500"}
+                >
+                  {_.capitalize(userProfile?.name)}
+                </Text>
+                <Text textColor={"gray.700"}>
+                  {_.capitalize(userProfile?.employeeType)}
+                </Text>
+                <HStack mt="4" gap={4}>
+                  <HStack>
+                    <Icon as={EmailIcon} />
+
+                    <Link>Email</Link>
+                  </HStack>
+                  <HStack>
+                    <Icon as={PhoneIcon} />
+                    <Link>Call</Link>
+                  </HStack>
+                </HStack>
               </Flex>
             </Flex>
-            <Code/>
-            <Flex>
-              <VStack>
-                <Flex>
-                  <Text >
+            <Code
+              bg="gray.50"
+              children="Personal Details"
+              p="2"
+              width={"94%"}
+              mx="4"
+              my="4"
+            />
+            <Flex
+              direction={"column"}
+              alignItems="start"
+              justifyContent={"start"}
+              p="4"
+            >
+              <VStack gap={"3"} alignItems="start" px="2">
+                <Flex direction={"column"}>
+                  <Text fontSize={"xs"} fontWeight="bold">
+                    About me :
                   </Text>
+                  <Text>{userProfile?.about}</Text>
                 </Flex>
-                <Flex >
-                  <Text >
+                <Flex direction={"column"}>
+                  <Text fontSize={"xs"} fontWeight="bold">
+                    Email :
                   </Text>
+                  <Text>{userProfile?.email}.</Text>
                 </Flex>
-                <Flex >
-                  <Text >
+                <Flex direction={"column"}>
+                  <Text fontSize={"xs"} fontWeight="bold">
+                    Phone Number :
                   </Text>
+                  <Text>{userProfile?.phoneNumber}.</Text>
                 </Flex>
-                <Flex>
-                  <Text >
-               
+                <Flex direction={"column"}>
+                  <Text fontSize={"xs"} fontWeight="bold">
+                    Joined Date :
                   </Text>
-                  <Text>
+                  <Text>{userProfile?.joinedDate}.</Text>
                 </Flex>
-                <Flex >
-                  <Text >
+                <Flex direction={"column"}>
+                  <Text fontSize={"xs"} fontWeight="bold">
+                    Salary :
                   </Text>
+                  <Text>{userProfile?.salary}.</Text>
                 </Flex>
-                <Flex >
+                <Flex direction={"column"}>
+                  <Text fontSize={"xs"} fontWeight="bold">
+                    Address :
+                  </Text>
+                  <Text>{userProfile?.address}.</Text>
                 </Flex>
               </VStack>
             </Flex>
@@ -98,7 +176,13 @@ const Employee = () => {
         </Grid>
       </Flex>
 
-      <AddEmployee/>
+      <AddEmployee
+        isModalOpen={addEmployeeModal}
+        setIsModalOpen={setAddEmployeeModal}
+        defaultData={initialFormData}
+        isUpdate={forUpdate}
+        setIsUpdate={setForUpdate}
+      />
     </React.Fragment>
   );
 };
