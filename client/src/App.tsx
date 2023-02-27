3f9c8cce6b8bc8fe0f291e41260a1749a9606756
bhanucheryala9 <cheryalabhanu12@gmail.com>
2023-02-27 00:16:34 -0500

    feat: installed necessary librarie on client side

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+import React, {
+  createContext,
+  Dispatch,
+  SetStateAction,
+  useState,
+} from "react";
+import "./App.css";
+
+interface CartContextProp {
+  isCartOpen: boolean;
+  setIsCartOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+function App() {
+  return (
+    <React.StrictMode>
+<div> app js
+  
+</div>
+    </React.StrictMode>
+  );
+}
+
+export default App;
