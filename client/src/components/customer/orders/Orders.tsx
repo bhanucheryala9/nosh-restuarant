096bbf904970f3412773e72a47f3203616212b64
sailakkineni <lsaishankar53@gmail.com>
2023-03-01 09:28:00 +0530

    implemented tab panels

--- client/src/components/customer/orders/Orders.tsx
+++ client/src/components/customer/orders/Orders.tsx
@@ -56,6 +56,63 @@ return(
             </Text>
           </Flex>
         </Flex>
+        <Tabs
+          variant="soft-rounded"
+          colorScheme="orange"
+          mt={{ base: "4", lg: "8" }}
+          mx={{ base: "4", lg: "10" }}
+          size={{ base: "sm", lg: "lg" }}
+          onChange={(index) => {
+            setSelectedCategory(Orders_Catergory[index]);
+            const redata = itemsData.filter(
+              (item: any) => item.category === Orders_Catergory[index]
+            );
+            setData(
+              Orders_Catergory[index] === "all"
+                ? itemsData.slice(0, 8)
+                : redata.slice(0, 8)
+            );
+          }}
+        >
+            <TabList>
+            {Orders_Catergory.map((item, index) => {
+              return <Tab key={index}>{_.capitalize(item)}</Tab>;
+            })}
+            </TabList>
+            <TabPanels my="8" rounded={"lg"} shadow="base">
+            {Orders_Catergory.map((index, item) => {
+              return (
+                <TabPanel bg={"white"} key={index}>
+                  <Flex justifyContent="space-between" alignItems="center">
+                    <Text
+                      fontSize={"2xl"}
+                      fontWeight="semibold"
+                      textColor={"orange.500"}
+                      mx="4"
+                      my="4"
+                    >
+                      Menu
+                    </Text>
+                    {/* <Input
+                      type="text"
+                      placeholder="search food"
+                      width="48"
+                      onChange={(e) => {
+                        setSearchFood(e.target.value);
+                      }}
+                    /> */}
+                  </Flex>
+
+          </TabPanel>
+              );
+            })}
+        </TabPanels>
+        </Tabs>
+
+
+
+
+       
         
         </Flex>
     </div>
