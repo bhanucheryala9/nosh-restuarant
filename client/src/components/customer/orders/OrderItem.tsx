59ef9996217fad1bd3ece10a9129fea3add6e6d2
SusmithaPusuluri <psusmitha184@gmail.com>
2023-03-05 07:28:24 -0400

    fixed for the isue

--- client/src/components/customer/orders/OrderItem.tsx
+++ client/src/components/customer/orders/OrderItem.tsx
@@ -15,6 +15,8 @@ import {
 import React, { useEffect, useState } from "react";
 import Biryani from "../../../assets/biryani.jpg";
 import { useCart } from "../../../contexts/CartContext";
+import CornerRibbon from "react-corner-ribbon";
+
 interface OrderItemsProps {
   id: string;
   productName: string;
@@ -23,7 +25,7 @@ interface OrderItemsProps {
   discount: number;
   isAvailable: boolean;
   tax: number;
-  url: string,
+  url: string;
   category: string;
   createdAt: string | Date;
 }
@@ -34,10 +36,20 @@ export interface OrderInfo {
   category: string;
   price: number;
   quantity: number;
+  url:string
 }
 const OrderItem = (props: OrderItemsProps) => {
-  const { id, productName, description, category, price, discount, isAvailable, tax, url  } =
-    props;
+  const {
+    id,
+    productName,
+    description,
+    category,
+    price,
+    discount,
+    isAvailable,
+    tax,
+    url,
+  } = props;
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState();
   const [image, setImage] = useState();
@@ -45,7 +57,9 @@ const OrderItem = (props: OrderItemsProps) => {
   useEffect(() => {
     const fetchImage = async () => {
       try {
-        const response = await import(`../../../assets/orders/${"chickenpakora.jpg"}`); // change relative path to suit your needs
+        const response = await import(
+          `../../../assets/orders/${"chickenpakora.jpg"}`
+        ); // change relative path to suit your needs
         setImage(response.default);
       } catch (err) {
       } finally {
@@ -59,11 +73,11 @@ const OrderItem = (props: OrderItemsProps) => {
   const onItemClicked = () => {
     const filterData =
       cartData.length > 0
-        ? cartData.findIndex((item: any) => item.productName === productName) !== -1
+        ? cartData.findIndex(
+            (item: any) => item.productName === productName
+          ) !== -1
           ? cartData.map((item: any) =>
-              item.id === id
-                ? { ...item, quantity: item.quantity + 1 }
-                : item
+              item.id === id ? { ...item, quantity: item.quantity + 1 } : item
             )
           : [
               ...cartData,
@@ -73,6 +87,7 @@ const OrderItem = (props: OrderItemsProps) => {
                 category: category,
                 price: price,
                 quantity: 1,
+                url: url
               },
             ]
         : [
@@ -82,13 +97,26 @@ const OrderItem = (props: OrderItemsProps) => {
               category: category,
               price: price,
               quantity: 1,
+              url: url
             },
           ];
-
+    console.log("********* data clicked for cart", filterData);
     setCartData(filterData);
   };
   return (
     <Card maxW={{ base: "full", lg: "sm" }} key={id}>
+      {!isAvailable && (
+        <CornerRibbon
+          position="top-right" // OPTIONAL, default as "top-right"
+          fontColor="#fff" // OPTIONAL, default as "#f0f0f0"
+          backgroundColor="#FF0000" // OPTIONAL, default as "#2c7"
+          containerStyle={{}} // OPTIONAL, style of the ribbon
+          style={{}} // OPTIONAL, style of ribbon content
+          className="" // OPTIONAL, css class of ribbon
+        >
+          NOT AVAILABLE
+        </CornerRibbon>
+      )}
       <CardBody>
         <Image
           filter="auto"
@@ -101,9 +129,7 @@ const OrderItem = (props: OrderItemsProps) => {
         />
         <Stack mt="6" spacing="3">
           <Heading size="md">{productName}</Heading>
-          <Text>
-            {description}
-          </Text>
+          <Text>{description}</Text>
         </Stack>
       </CardBody>
       <CardFooter justifyContent="space-between" alignItems={"center"}>
@@ -112,7 +138,8 @@ const OrderItem = (props: OrderItemsProps) => {
         </Text>
         <Button
           variant={"solid"}
-          colorScheme="orange"
+          colorScheme={isAvailable ? "orange" : "gray"}
+          disabled={!isAvailable}
           rounded={"full"}
           onClick={onItemClicked}
         >
