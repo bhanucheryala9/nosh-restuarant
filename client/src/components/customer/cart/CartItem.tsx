0bb42a74a3d0f15217160aff8d1cf7a99ade180d
Sreeya Meela <sreeyameela@Sreeyas-MacBook-Air.local>
2023-03-07 03:00:00 -0400

    cart item display

--- client/src/components/customer/cart/CartItem.tsx
+++ client/src/components/customer/cart/CartItem.tsx
@@ -14,35 +14,54 @@ const CartItem = (props: CartItemProps) => {
   };
 
   return (
+    <Flex my="4" key={item.id}>
     <Flex>
-      <Flex>
-        <Image
-        />
-      </Flex>
-      <Flex>
-        <HStack>
-          <Text >
-
-          </Text>
-          <Text>
-        
-          </Text>
-        </HStack>
-        <HStack >
-          <Text ></Text>
-          <Text>
-            <InputGroup
-            >
-              <InputLeftAddon
-              />
-              <Input />
-              <InputRightAddon
-              />
-            </InputGroup>
-          </Text>
-        </HStack>
-      </Flex>
+      <Image
+        src={item?.url !== "" ? item?.url : faker.image.food()}
+        maxH={14}
+        maxW={14}
+        borderRadius="lg"
+      />
     </Flex>
+    <Flex mx="4" direction={"column"} width="100%">
+      <HStack justifyContent={"space-between"}>
+        <Text fontSize={"lg"} fontWeight="semibold">
+          {item.productName}
+        </Text>
+        <Text fontSize={"lg"} textColor="orange.600" fontWeight={"semibold"}>
+          ${(item.price * item.quantity).toFixed(2)}
+        </Text>
+      </HStack>
+      <HStack justifyContent={"space-between"}>
+        <Text fontSize={"sm"}>X {item.quantity}</Text>
+        <Text>
+          <InputGroup
+            size="sm"
+            colorScheme={"orange"}
+            borderColor="orange.500"
+          >
+            <InputLeftAddon
+              cursor="pointer"
+              children="-"
+              onClick={() => prepareData("sub")}
+            />
+            <Input
+              type={"number"}
+              defaultValue={item.quantity}
+              value={item.quantity}
+              w={10}
+            />
+            <InputRightAddon
+              cursor="pointer"
+              children="+"
+              onClick={() => prepareData("add")}
+            />
+          </InputGroup>
+        </Text>
+      </HStack>
+    </Flex>
+  </Flex>
+
   );
 };
 
