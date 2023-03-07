16d5849c819e41bb5bb33e3453e237c349d54092
Sreeya Meela <sreeyameela@Sreeyas-MacBook-Air.local>
2023-03-07 01:30:00 -0400

    cart item skeleton

--- client/src/components/customer/cart/CartItem.tsx
+++ client/src/components/customer/cart/CartItem.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
 
-export default function CartItem() {
-  return (
-    <div>
-      
-    </div>
-  )
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { OrderInfo } from "../orders/OrderItem";
+
+interface CartItemProps {
+  item: OrderInfo;
+  setCartInfo: React.Dispatch<React.SetStateAction<OrderInfo[]>>;
+  cartInfo: OrderInfo[];
 }
+
+const CartItem = (props: CartItemProps) => {
+
+  };
+
+  return (
+    <Flex>
+      <Flex>
+        <Image
+        />
+      </Flex>
+      <Flex>
+        <HStack>
+          <Text >
+
+          </Text>
+          <Text>
+        
+          </Text>
+        </HStack>
+        <HStack >
+          <Text ></Text>
+          <Text>
+            <InputGroup
+            >
+              <InputLeftAddon
+              />
+              <Input />
+              <InputRightAddon
+              />
+            </InputGroup>
+          </Text>
+        </HStack>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default CartItem;
