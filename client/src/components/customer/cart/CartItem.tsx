a5a56039d6076653e8d31b011ed7d9a3ee8fe64f
Sreeya Meela <sreeyameela@Sreeyas-MacBook-Air.local>
2023-03-08 03:30:00 -0400

    add functionality updated

--- client/src/components/customer/cart/CartItem.tsx
+++ client/src/components/customer/cart/CartItem.tsx
@@ -38,63 +38,68 @@ const CartItem = (props: CartItemProps) => {
     } else {
       updatedData = cartInfo?.map((product) => {
         if (product.productName === item.productName) {
-        } 
+          if (product.quantity <= 0) {
+            return { ...product, quantity: 0 };
+          } else {
+            return { ...product, quantity: item.quantity - 1 };
+          }
+        } else {
+          return product;
+        }
       });
     }
     localStorage.setItem("orders", JSON.stringify(updatedData))
     setCartInfo(updatedData);
-
   };
 
   return (
     <Flex my="4" key={item.id}>
-    <Flex>
-      <Image
-        src={item?.url !== "" ? item?.url : faker.image.food()}
-        maxH={14}
-        maxW={14}
-        borderRadius="lg"
-      />
+      <Flex>
+        <Image
+          src={item?.url !== "" ? item?.url : faker.image.food()}
+          maxH={14}
+          maxW={14}
+          borderRadius="lg"
+        />
+      </Flex>
+      <Flex mx="4" direction={"column"} width="100%">
+        <HStack justifyContent={"space-between"}>
+          <Text fontSize={"lg"} fontWeight="semibold">
+            {item.productName}
+          </Text>
+          <Text fontSize={"lg"} textColor="orange.600" fontWeight={"semibold"}>
+            ${(item.price * item.quantity).toFixed(2)}
+          </Text>
+        </HStack>
+        <HStack justifyContent={"space-between"}>
+          <Text fontSize={"sm"}>X {item.quantity}</Text>
+          <Text>
+            <InputGroup
+              size="sm"
+              colorScheme={"orange"}
+              borderColor="orange.500"
+            >
+              <InputLeftAddon
+                cursor="pointer"
+                children="-"
+                onClick={() => prepareData("sub")}
+              />
+              <Input
+                type={"number"}
+                defaultValue={item.quantity}
+                value={item.quantity}
+                w={10}
+              />
+              <InputRightAddon
+                cursor="pointer"
+                children="+"
+                onClick={() => prepareData("add")}
+              />
+            </InputGroup>
+          </Text>
+        </HStack>
+      </Flex>
     </Flex>
-    <Flex mx="4" direction={"column"} width="100%">
-      <HStack justifyContent={"space-between"}>
-        <Text fontSize={"lg"} fontWeight="semibold">
-          {item.productName}
-        </Text>
-        <Text fontSize={"lg"} textColor="orange.600" fontWeight={"semibold"}>
-          ${(item.price * item.quantity).toFixed(2)}
-        </Text>
-      </HStack>
-      <HStack justifyContent={"space-between"}>
-        <Text fontSize={"sm"}>X {item.quantity}</Text>
-        <Text>
-          <InputGroup
-            size="sm"
-            colorScheme={"orange"}
-            borderColor="orange.500"
-          >
-            <InputLeftAddon
-              cursor="pointer"
-              children="-"
-              onClick={() => prepareData("sub")}
-            />
-            <Input
-              type={"number"}
-              defaultValue={item.quantity}
-              value={item.quantity}
-              w={10}
-            />
-            <InputRightAddon
-              cursor="pointer"
-              children="+"
-              onClick={() => prepareData("add")}
-            />
-          </InputGroup>
-        </Text>
-      </HStack>
-    </Flex>
-  </Flex>
-
   );
 };
 
