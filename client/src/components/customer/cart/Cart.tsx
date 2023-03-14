5a1def1cf6a041b88a0fd25a1bb1f93b8a49d1e8
Sreeya Meela <sreeyameela@Sreeyas-MacBook-Air.local>
2023-03-14 06:00:00 -0400

    cart style updated

--- client/src/components/customer/cart/Cart.tsx
+++ client/src/components/customer/cart/Cart.tsx
@@ -25,51 +25,81 @@ const Cart = () => {
   return (
     <div>
       <Drawer
+        onClose={() => setIsCartOpen(false)}
+        isOpen={isCartOpen}
+        size={"md"}
       >
         <DrawerOverlay />
         <DrawerContent>
-          <DrawerCloseButton />
-          <DrawerHeader></DrawerHeader>
+          <DrawerCloseButton _focus={{ outline: "none" }} />
+          <DrawerHeader>{` Your Cart`}</DrawerHeader>
           <DrawerBody>
-            <Flex>
-              <Text>
-              
+            <Flex
+              border={"2px solid"}
+              borderColor="orange.300"
+              minWidth={"70%"}
+              borderRadius={"md"}
+              bg="orange.100"
+              justifyContent={"center"}
+              direction="column"
+              px="6"
+            >
+              <Text mt="4" fontWeight={"semibold"}>
+                Your Dashboard
               </Text>
               <Flex
-              ><Text fontSize={"lg"} textColor="white">
-              Balance
-            </Text>
-            <Text fontSize={"4xl"} fontWeight="semibold" textColor="white">
-              $200
-            </Text>
+                bg="linear-gradient(-60deg, #ff5858 0%, #f09819 100%)"
+                px="6"
+                py="6"
+                maxH={"40"}
+                minW="80"
+                borderRadius={"xl"}
+                direction="column"
+                my="3"
+              >
+                <Text fontSize={"lg"} textColor="white">
+                  Balance
+                </Text>
+                <Text fontSize={"4xl"} fontWeight="semibold" textColor="white">
+                  $200
+                </Text>
               </Flex>
-              <Divider  />
-
-                return (
-                  <CartItem
-                  />
+              <Divider mb="4" />
+             
                 );
-        
+              })}
 
-              <Divider />
-              <Flex></Flex>
-                <HStack >
-                  <Text >
+              <Divider my="6" />
+              <Flex mb="6" direction={"column"} pr="3">
+                <HStack justifyContent={"space-between"}>
+                  <Text fontSize={"sm"} textColor="gray.700">
+                    Tax
                   </Text>
-                  <Text ></Text>
+                  <Text fontSize={"sm"}>${(tax).toFixed(2)}</Text>
                 </HStack>
-                <HStack >
-                  <Text >
+                <HStack justifyContent={"space-between"}>
+                  <Text fontSize={"sm"} textColor="gray.700">
+                    Sub total
                   </Text>
-                  <Text></Text>
+                  <Text fontSize={"sm"}>${(totalAmount).toFixed(2)}</Text>
                 </HStack>
-                <HStack>
-                  <Text></Text>
-                  <Text>
+                <HStack justifyContent={"space-between"} mt="2">
+                  <Text>Total</Text>
+                  <Text
+                    textColor={"orange.500"}
+                    fontWeight="semibold"
+                    fontSize={"2xl"}
+                  >
+                    ${(totalAmount+ tax).toFixed(2)}
                   </Text>
                 </HStack>
                 <Button
+                  colorScheme={"orange"}
+                  width="100%"
+                  mt="3"
+                  onClick={onSubmitClicked}
                 >
+                  Proceed to Pay
                 </Button>
               </Flex>
             </Flex>
