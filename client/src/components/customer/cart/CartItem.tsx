bc904ad02cdf43548b98197431ef8c091ea5799b
SusmithaPusuluri <psusmitha184@gmail.com>
2023-03-15 15:38:14 -0400

    added the updated changes of cart

--- client/src/components/customer/cart/CartItem.tsx
+++ client/src/components/customer/cart/CartItem.tsx
@@ -12,51 +12,63 @@ import React, { useState } from "react";
 import { faker } from "@faker-js/faker";
 import { OrderInfo } from "../orders/OrderItem";
 
-interface CartItemProps{
+interface CartItemProps {
   item: OrderInfo;
-  setCartInfo: React.Dispatch<React.SetStateAction<OrderInfo[]>>
-  cartInfo: OrderInfo[]
+  setCartInfo: React.Dispatch<React.SetStateAction<OrderInfo[]>>;
+  cartInfo: OrderInfo[];
 }
 
 const CartItem = (props: CartItemProps) => {
-  const { item, setCartInfo, cartInfo} = props;
+  const { item, setCartInfo, cartInfo } = props;
 
-  const prepareData = (option: string) =>{
+  const prepareData = (option: string) => {
     let updatedData;
-    if(option==='add'){
-       updatedData = cartInfo?.map((product)=> {
-        if (product.productName === item.productName){
-          return {...product, quantity: item.quantity+1}
-        }else{
-          return product
+    if (option === "add") {
+      updatedData = cartInfo?.map((product) => {
+        if (product.productName === item.productName) {
+          if (product.quantity < 0) {
+            return { ...product, quantity: 0 };
+          } else {
+            return { ...product, quantity: item.quantity + 1 };
+          }
+        } else {
+          return product;
         }
-      })
-
-      console.log("************ after adding", updatedData)
-    }else{
-      updatedData = cartInfo?.map((product)=> {
-        if (product.productName === item.productName){
-          return {...product, quantity: item.quantity-1}
-        }else{
-          return product
+      });
+    } else {
+      updatedData = cartInfo?.map((product) => {
+        if (product.productName === item.productName) {
+          if (product.quantity <= 0) {
+            return { ...product, quantity: 0 };
+          } else {
+            return { ...product, quantity: item.quantity - 1 };
+          }
+        } else {
+          return product;
         }
-    })}
+      });
+    }
+    localStorage.setItem("orders", JSON.stringify(updatedData))
     setCartInfo(updatedData);
-    
-  }
+  };
 
   return (
     <Flex my="4" key={item.id}>
       <Flex>
-        <Image src={faker.image.food()} maxH={14} maxW={14} borderRadius="lg" />
+        <Image
+          src={item?.url !== "" ? item?.url : faker.image.food()}
+          maxH={14}
+          maxW={14}
+          borderRadius="lg"
+        />
       </Flex>
       <Flex mx="4" direction={"column"} width="100%">
         <HStack justifyContent={"space-between"}>
           <Text fontSize={"lg"} fontWeight="semibold">
-           {item.productName}
+            {item.productName}
           </Text>
           <Text fontSize={"lg"} textColor="orange.600" fontWeight={"semibold"}>
-            ${item.price}
+            ${(item.price * item.quantity).toFixed(2)}
           </Text>
         </HStack>
         <HStack justifyContent={"space-between"}>
@@ -67,9 +79,22 @@ const CartItem = (props: CartItemProps) => {
               colorScheme={"orange"}
               borderColor="orange.500"
             >
-              <InputLeftAddon children="-" onClick={()=>prepareData("sub")}/>
-              <Input type={"number"} defaultValue={item.quantity} value={item.quantity} w={10} />
-              <InputRightAddon children="+" onClick={()=>prepareData("add")} />
+              <InputLeftAddon
+                cursor="pointer"
+                children="-"
+                onClick={() => prepareData("sub")}
+              />
+              <Input
+                type={"number"}
+                defaultValue={item.quantity}
+                value={item.quantity}
+                w={10}
+              />
+              <InputRightAddon
+                cursor="pointer"
+                children="+"
+                onClick={() => prepareData("add")}
+              />
             </InputGroup>
           </Text>
         </HStack>
