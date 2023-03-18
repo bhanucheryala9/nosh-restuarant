c4327ba3140943d1d03293559b53c7af66d96bfa
SusmithaPusuluri <psusmitha184@gmail.com>
2023-03-18 15:18:28 -0400

    added some changed in cancel order

--- client/src/components/employee/dashboard/EmployeeDashboard.tsx
+++ client/src/components/employee/dashboard/EmployeeDashboard.tsx
@@ -116,23 +116,157 @@ import {
     
   
     return (
-      <React.Fragment>
-       <Flex mt="4" direction={"column"} alignItems={"end"}>
-                <Text fontSize={"lg"} fontWeight={"semibold"}>
-                  Total Price: $
-                  {((selectedOrder?.totalAmount || 0) / 100).toFixed(2)}
-                </Text>
-                <Button
-                  colorScheme="orange"
-                  mt="6"
-                  maxW={"40"}
-                  onClick={onStatusUpdateClicked}
+        <React.Fragment>
+          <Flex bg="white" rounded={"xl"} my="4" mx="6" px="6" py="6">
+            <Grid
+              templateRows="repeat(1, 1fr)"
+              templateColumns="repeat(4, 1fr)"
+              gap={4}
+              width={"100%"}
+            >
+              <GridItem
+                rowSpan={1}
+                colSpan={1}
+                bg="white"
+                rounded={"md"}
+                p="4"
+                width={"100%"}
+              >
+                <Text
+                  fontSize={"2xl"}
+                  fontWeight={"semibold"}
+                  mb="4"
+                  textColor={"orange.600"}
                 >
-                  Cancel Order
-                </Button>
-              </Flex>
-      </React.Fragment>
-    );
+                  Orders List
+                </Text>
+                
+                <VStack mt="6" w={"100%"}>
+                  {eordersData
+                    ?.filter((item) => item.orderStatus === segmentValue)
+                    ?.map((data) => {
+                      return (
+                        <Flex
+                          bg="gray.100"
+                          py="3"
+                          px="4"
+                          borderRadius={"lg"}
+                          width={"100%"}
+                          cursor={"pointer"}
+                          justifyContent={"space-between"}
+                          onClick={() => setSelectedOrder(data)}
+                        >
+                          <VStack justifyContent={"start"} alignItems={"start"}>
+                            <Text textColor={"black"}>
+                              ORDER ID{" "}
+                              <b>#{data.orderId.toString().toUpperCase()}</b>
+                            </Text>
+                            <Text fontSize={"xs"} textColor={"gray.500"}>
+                              {" "}
+                              a few min ago
+                            </Text>
+                          </VStack>
+                          <Text fontSize={"xl"} fontWeight={"bold"}>
+                            ${((data.totalAmount || 0) / 100).toFixed(2)}
+                          </Text>
+                        </Flex>
+                      );
+                    })}
+                </VStack>
+              </GridItem>
+    
+              <GridItem
+                rowSpan={1}
+                colSpan={3}
+                bg="gray.100"
+                rounded={"xl"}
+                py="4"
+                px="8"
+              >
+                <Text
+                  fontSize={"2xl"}
+                  fontWeight={"semibold"}
+                  textColor={"orange.600"}
+                >
+                  Order Info{" "}
+                </Text>
+                <HStack
+                  divider={<StackDivider borderColor="gray.300" />}
+                  gap={4}
+                  my="4"
+                  width={"100%"}
+                >
+                  <VStack
+                    minW={"30%"}
+                    alignItems={"start"}
+                    gap={0}
+                    justifyContent={"start"}
+                    justifyItems={"start"}
+                  >
+                    <Text fontSize={"xs"} textColor={"gray.600"}>
+                      Order ID
+                    </Text>
+                    <Text fontSize={"md"} fontWeight={"semibold"}>
+                      {selectedOrder?.orderId?.toUpperCase()}
+                    </Text>
+                  </VStack>
+                  <VStack minW={"30%"} alignItems={"start"}>
+                    <Text fontSize={"xs"} textColor={"gray.600"}>
+                      Address
+                    </Text>
+                    <Text fontSize={"md"} fontWeight={"semibold"}>
+                      {selectedOrder?.address?.addressLine1},{" "}
+                      {selectedOrder?.address.city}
+                    </Text>
+                  </VStack>
+                  <VStack minW={"35%"} alignItems={"start"}>
+                    <Text fontSize={"xs"} textColor={"gray.600"}>
+                      Personal Details
+                    </Text>
+                    <Text fontSize={"md"} fontWeight={"semibold"}>
+                      {selectedOrder?.lastName}
+                    </Text>
+                  </VStack>
+                </HStack>
+                <Flex width={"100%"} mt="10" direction={"column"}>
+                  {selectedOrder?.orderDetails?.map((item) => {
+                    return (
+                      <VStack width={"100%"} py="2">
+                        <Flex justifyContent={"space-between"} width={"100%"}>
+                          <Text fontSize={"large"} fontWeight={"semibold"}>
+                            {item.productName}
+                          </Text>
+                          <HStack gap={20}>
+                            <Text fontSize={"md"}>Quantity: {item.quantity}</Text>
+                            <Text fontSize={"lg"} fontWeight={"semibold"}>
+                              Price: ${item.price}
+                            </Text>
+                          </HStack>
+                        </Flex>
+                        <Divider />
+                      </VStack>
+                    );
+                  })}
+                  <Flex mt="4" direction={"column"} alignItems={"end"}>
+                    <Text fontSize={"lg"} fontWeight={"semibold"}>
+                      Total Price: $
+                      {((selectedOrder?.totalAmount || 0) / 100).toFixed(2)}
+                    </Text>
+                    <Button
+                      colorScheme="orange"
+                      mt="6"
+                      maxW={"40"}
+                      onClick={onStatusUpdateClicked}
+                    >
+                      Cancel Order
+                    </Button>
+                  </Flex>
+                </Flex>
+              </GridItem>
+            </Grid>
+          </Flex>
+        </React.Fragment>
+      );
   };
   
   export default EmployeeDashboard;
