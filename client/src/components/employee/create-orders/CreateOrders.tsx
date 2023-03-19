2c01530563a338cc51df842255b23f52439b193a
sailakkineni <lsaishankar53@gmail.com>
2023-03-19 12:25:00 +0530

    Made final changes and improvements for customizations

--- client/src/components/employee/create-orders/CreateOrders.tsx
+++ client/src/components/employee/create-orders/CreateOrders.tsx
@@ -32,7 +32,48 @@ const CreateOrders = () => {
       tax: 0,
     });
     const navigate = useNavigate();
+    const navigate = useNavigate();
 
+    const handleCart = (data: any, operation: string) => {
+      const updatedData = orders?.map((item: any) => {
+        if (operation === "add") {
+          if (item.productName === data.productName) {
+            return {
+              ...item,
+              quantity: item.quantity + 1,
+            };
+          } else {
+            return item;
+          }
+        } else {
+          if (item.productName === data.productName) {
+            return {
+              ...item,
+              quantity: item.quantity - 1,
+            };
+          } else {
+            return item;
+          }
+        }
+      });
+      const amount = updatedData?.reduce((acc, curr) => {
+        return acc + curr.quantity * curr.price;
+      }, 0);
+  
+      setOrders(updatedData as any);
+      setAmount({
+        total: amount,
+        tax: Number((amount / 8).toFixed(2)),
+      });
+    };
+  
+    const prepareData = (payload: any) => {
+      const data1 = payload.map((item: any) => {
+        return { ...item, quantity: 0 };
+      });
+      return data1;
+    };
+  
     return(
     <Flex direction={"column"} mx="6" my="6">
       <Flex justifyContent={"space-between"} mb="3">
@@ -44,29 +85,163 @@ const CreateOrders = () => {
         </Button>
         </Flex>
         <Grid
-        
+        templateRows="repeat(1, 1fr)"
+        templateColumns="repeat(4, 1fr)"
+        gap={4}
       >
-
-      
-       <GridItem>
-        <Tabs>
-            <TabList>
+        <GridItem
+          rowSpan={1}
+          colSpan={3}
+          bg="white"
+          rounded={"md"}
+          shadow={"base"}
+          p="4"
+        >
+          <Tabs
+            colorScheme="orange"
+            mb="4"
+            onChange={(index) => {
+              setSelectedCategory(Orders_Catergory[index]);
+              const redata = itemsData.filter(
+                (item: any) => item.category === Orders_Catergory[index]
+              );
+              setOrders(
+                Orders_Catergory[index] === "all"
+                  ? itemsData.slice(0, 8)
+                  : redata.slice(0, 8)
+              );
+            }}
+          >
+            <TabList mb="4">
+              {Orders_Catergory.map((item, index) => {
+                return <Tab key={index}>{_.capitalize(item)}</Tab>;
+              })}
             </TabList>
-        <TabPanels>
-        {Orders_Catergory.map((item, index) => {
-            return(
-            <TabPanel>
-                <Grid>
 
-                </Grid>
-            </TabPanel>
-            );
-              
-              })};
-        </TabPanels>
-          
-        ></GridItem>
-        </Grid>
+            <TabPanels>
+
+                return (
+                  <TabPanel>
+                    <Grid
+                      templateRows="repeat(2, 1fr)"
+                      templateColumns="repeat(4, 1fr)"
+                      gap={6}
+                      width={"100%"}
+                    >
+                      {orders?.map((item) => {
+                        return (
+                          <GridItem rowSpan={1} colSpan={1}>
+                            {orderItem(item)}
+                          </GridItem>
+                        );
+                      })}
+                    </Grid>
+                  </TabPanel>
+                );
+              })}
+            </TabPanels>
+          </Tabs>
+        </GridItem>
+        <GridItem
+          rowSpan={1}
+          colSpan={1}
+          bg="white"
+          p="4"
+          borderRadius={"md"}
+          shadow={"base"}
+          justifyContent="center"
+        >
+          <Text
+            fontSize={"xl"}
+            fontWeight={"semibold"}
+            textColor={"orange.500"}
+            mb="4"
+          >
+            Find your cart here
+          </Text>
+          <Flex justifyContent="center" direction="column" alignItems="center">
+            {orders.filter((item: any) => item.quantity !== 0).length !== 0 ? (
+              <Flex direction="column" width="100%">
+                {orders
+                  ?.filter((item: any) => item.quantity !== 0)
+                  ?.map((data: any) => {
+                    return (
+                      <Flex
+                        bg="gray.100"
+                        py="3"
+                        px="4"
+                        direction={"column"}
+                        borderRadius={"lg"}
+                        mt="3"
+                        w="100%"
+                      >
+                        <Flex
+                          justifyContent="space-between"
+                          alignItems="center"
+                        >
+                          <Flex>
+                            <Image
+                              src={data.url}
+                              width={"80px"}
+                              height={"80px"}
+                              borderRadius={"lg"}
+                            />
+                            <Flex direction={"column"} ml="6">
+                              <Text
+                                fontSize={"lg"}
+                                fontWeight={"semibold"}
+                                my="2"
+                              >
+                                {_.capitalize(data.productName)}
+                              </Text>
+                              <Text>Quantity: {data.quantity}</Text>
+                            </Flex>
+                          </Flex>
+                          <Flex>
+                            <Text
+                              textColor="orange.500"
+                              fontWeight="semibold"
+                              fontSize="xl"
+                            >
+                              ${(data.quantity * data.price).toFixed(2)}
+                            </Text>
+                          </Flex>
+                        </Flex>
+                      </Flex>
+                    );
+                  })}
+                <Divider mt="6" />
+                <Text my="3">Specify your customization</Text>
+                <Textarea />
+                <Divider mt="6" />
+                <VStack mt="3" width="100%" px="3">
+                  <Flex justifyContent="space-between" width="100%">
+                    <Text fontWeight="semibold">Tax</Text>
+                    <Text>${amount.tax}</Text>
+                  </Flex>
+                  <Flex justifyContent="space-between" width="100%">
+                    <Text fontWeight="semibold">Sub Total</Text>
+                    <Text>${amount.total}</Text>
+                  </Flex>
+                </VStack>
+                <Divider mt="3" />
+                <Flex justifyContent="space-between" width="100%" px="3">
+                  <Text fontWeight="semibold">Total</Text>
+                  <Text>${amount.total + amount.tax}</Text>
+                </Flex>
+              </Flex>
+            ) : (
+              <Flex mt="16">
+                <Empty
+                  image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
+                  imageStyle={{ height: 100 }}
+                  description="No Cart Data..."
+                />
+              </Flex>
+            )}
+          </Flex>
+        </GridItem>
+      </Grid>
     </Flex>
 
 
