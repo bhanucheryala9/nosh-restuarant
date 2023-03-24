72baceb95183d8dae6b0523b3eec6256e8d14a40
Akhil <apabbathireddy@albany.edu>
2023-03-24 18:15:00 +0530

    react component rewards

--- client/src/components/admin/rewards/Rewards.tsx
+++ client/src/components/admin/rewards/Rewards.tsx
@@ -75,7 +75,7 @@ const Rewards = () => {
         });
       });
   };
-  
+
   const prepareStatsData = (data: any) => {
     const stats = data?.reduce(
       (acc: any, current: any) => {
@@ -154,3 +154,162 @@ const Rewards = () => {
       ),
     },
   ];
+
+  const prepareData = (data: RewardsRequestPayload[]) => {
+    const formattedData = data.reduce(
+      (accumulator: any, currentValue: RewardsRequestPayload) => {
+        if (currentValue.appliedCategory) {
+          return [
+            ...accumulator,
+            {
+              key: currentValue.id,
+              ...currentValue,
+            },
+          ];
+        } else {
+          return [
+            ...accumulator,
+            {
+              key: currentValue.id,
+              ...currentValue,
+              appliedCategory: "all",
+            },
+          ];
+        }
+      },
+      []
+    );
+    return formattedData;
+  };
+  useEffect(() => {
+    axios
+      .get("http://34.235.166.147:5000/api/admin/v1/get-rewards")
+      .then((response) => {
+        setRewardsData(prepareData(response.data.rewards));
+        prepareStatsData(prepareData(response.data.rewards));
+      })
+      .catch((error) => {});
+  }, [showCreateRewardModal]);
+
+
+  const StatusProps = (prop: string) => {
+    if (prop === "total") return "Total";
+    else if (prop === "active") return "Active";
+    else if (prop === "inactive") return "In Active";
+    else if (prop === "scheduled") return "Scheduled";
+  }
+
+  const gradientColor = [
+    "linear-gradient(to top, #30c7ec 47%, #46aef7 70%)",
+    "linear-gradient(to top, #2b5876 -30%, #4e4376 70%)",
+    "linear-gradient(to top, #0ba360 0%, #3cba92 40%)",
+    "linear-gradient(to top, #ff0844 -30%, #ffb199 90%)",
+  ];
+
+  const getStatusComponent = () => {
+    return Object.entries(statsData || {}).map((item: any, index) => {
+      return (
+        <GridItem colSpan={1} rowSpan={1} key={index}>
+          <Flex
+            bg="white"
+            shadow="sm"
+            px="8"
+            py="5"
+            rounded="md"
+            alignItems={"center"}
+            bgGradient={gradientColor[index]}
+            textColor={"white"}
+            minH="36"
+            maxW={{ base: "full" }}
+            position={"relative"}
+            backdropFilter="brightness(10%)"
+          >
+            <LineChart
+              width={280}
+              height={150}
+              data={statsChartData}
+              margin={{ top: 5, bottom: 5 }}
+            >
+              <XAxis dataKey="name" hide={true} />
+              <YAxis hide={true} />
+              <Line
+                type="monotone"
+                dataKey="pv"
+                stroke="#cccdc6"
+                strokeWidth={2}
+                strokeOpacity={0.3}
+                dot={{ r: 2 }}
+                opacity={50}
+              />
+            </LineChart>
+            <HStack position={"absolute"} mx={10} gap={3}>
+              <Icon as={ChatIcon} boxSize={14} textShadow="0 0 10px black" />
+              <Stat>
+                <StatLabel fontSize={"xl"} textShadow="0 0 10px black">
+                  {StatusProps(item[0])}
+                </StatLabel>
+                <StatNumber fontSize={"4xl"} textShadow="0 0 10px black">
+                  {item[1]}
+                </StatNumber>
+              </Stat>
+            </HStack>
+          </Flex>
+        </GridItem>
+      );
+    });
+  };
+  return (
+    <React.Fragment>
+      <Flex mx={{ base: "4", lg: "10" }} my="6" direction={"column"}>
+        <Flex justifyContent={"space-between"}>
+          <Text fontSize={{ base: "lg", lg: "xl" }} fontWeight="bold">
+            Offers and Rewards
+          </Text>
+          <Button
+            size={{ base: "sm", lg: "md" }}
+            colorScheme={"orange"}
+            onClick={() => setShowCreateRewardModal(true)}
+          >
+            {" "}
+            Create Coupons
+          </Button>
+        </Flex>
+
+        <Grid
+          my="4"
+          templateRows={{ base: "repeat(4, 1fr)", lg: "repeat(1, 1fr)" }}
+          templateColumns={{ base: "repeat(1, 1fr)", lg: "repeat(4, 1fr)" }}
+          gap={{ base: 3, lg: 4 }}
+        >
+          {getStatusComponent()}
+        </Grid>
+
+        <Flex bg="white" p="6" mt="4" shadow={"sm"} rounded="sm">
+          <Table
+            onRow={(record, rowIndex) => {
+              return {
+                onClick: (event) => {
+                  // setUserProfile(record);
+                },
+              };
+            }}
+            style={{ width: "100%" }}
+            scroll={{ x: 400 }}
+            size="large"
+            columns={columns}
+            dataSource={rewardsData}
+          />
+        </Flex>
+      </Flex>
+      <CreateReward
+        isModalOpen={showCreateRewardModal}
+        setIsModalOpen={setShowCreateRewardModal}
+        forUpdate={forUpdate}
+        setForUpdate={setForUpdate}
+        defaultData={toUpdateData}
+      />
+    </React.Fragment>
+  );
+};
+
+export default Rewards;
