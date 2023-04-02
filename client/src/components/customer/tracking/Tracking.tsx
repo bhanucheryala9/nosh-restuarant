646bf931a138e22386d6091f851abde174df757e
Sreeya Meela <sreeyameela@Sreeyas-MacBook-Air.local>
2023-04-02 08:30:00 -0400

    tracking skeleton

--- client/src/components/customer/tracking/Tracking.tsx
+++ client/src/components/customer/tracking/Tracking.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
 
-export default function Tracking() {
+import React, { createRef, useCallback, useEffect, useState } from "react";
+const Tracking = () => {
+
+
+
+
+  }
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <React.Fragment>
+      <Flex >
+        <Flex>
+          <Text >
+            Check Your Status
+          </Text>
+          <Divider />
+          <HStack >
+            <Text>
+            </Text>
+            <Text> </Text>
+          </HStack>
+          <Flex>
+          
+          
+          
+        </Flex>
+      </Flex>
+    </React.Fragment>
+  );
+};
+
+export default Tracking;
