53d9d97e7221de88da33a4eea9054642336e984b
Sreeya Meela <sreeyameela@Sreeyas-MacBook-Air.local>
2023-04-02 08:30:00 -0400

    status props added

--- client/src/components/customer/tracking/Tracking.tsx
+++ client/src/components/customer/tracking/Tracking.tsx
@@ -3,6 +3,52 @@ import React, { createRef, useCallback, useEffect, useState } from "react";
 const Tracking = () => {
 
 
+  const statusProps = [
+    {
+      title: (
+        <Flex alignItems="center">
+          <Image src={orderPlaced} h="50px" w="50px" />{" "}
+          <Text ml="2" fontWeight="semibold" fontSize="md">
+            {" "}
+            Order Placed
+          </Text>
+        </Flex>
+      ),
+    },
+    {
+      title: (
+        <Flex alignItems="center">
+          <Image src={accepted} h="50px" w="50px" />{" "}
+          <Text ml="2" fontWeight="semibold">
+            {" "}
+            Order Accepted
+          </Text>
+        </Flex>
+      ),
+    },
+    {
+      title: (
+        <Flex alignItems="center">
+          <Image src={preparing} h="50px" w="50px" />{" "}
+          <Text ml="2" fontWeight="semibold">
+            {" "}
+            Preparing
+          </Text>
+        </Flex>
+      ),
+    },
+    {
+      title: (
+        <Flex alignItems="center">
+          <Image src={ready} h="50px" w="50px" />{" "}
+          <Text ml="2" fontWeight="semibold">
+            {" "}
+            Ready
+          </Text>
+        </Flex>
+      ),
+    },
+  ];
 
 
   }
