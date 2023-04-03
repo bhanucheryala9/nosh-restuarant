d6feed50c32a23efcf9a42694204878d9d353045
Sreeya Meela <sreeyameela@Sreeyas-MacBook-Air.local>
2023-04-03 07:30:00 -0400

    steps on display

--- client/src/components/customer/tracking/Tracking.tsx
+++ client/src/components/customer/tracking/Tracking.tsx
@@ -81,10 +81,37 @@ const Tracking = () => {
             </Text>
             <Text> ORDER ID: #{(trackorder as any)?.orderId}</Text>
         </HStack>
-   
+        <Flex mt="16" width="100%">
+            {stepDetails !== -1 ? (
+              <Steps
+                style={{ width: "100%" }}
+                progressDot
+                current={stepDetails}
+                direction="horizontal"
+                items={statusProps}
+              />
+            ) : (
+              <Flex w="550px" direction="column" alignItems="center" mt="3">
+                <ImCancelCircle fontSize="40px" color="red" />
+                <Text
+                  mt="6"
+                  fontSize="lg"
+                  textColor="red.500"
+                  fontWeight="semibold"
+                >
+                  {" "}
+                  Order Cancelled
+                </Text>
+                <Text>Please contact restaurant...! </Text>
+              </Flex>
+            )}
+          </Flex>
+          <Button colorScheme="orange" mt="10" mb="6" onClick={onDaskClicked}>
+            Back to Home Page
+          </Button>
+        </Flex>
       </Flex>
-    </Flex>
-  </React.Fragment>
+    </React.Fragment>
   );
 };
 
