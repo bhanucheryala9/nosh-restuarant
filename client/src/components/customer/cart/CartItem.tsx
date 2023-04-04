07a09b3c0f5b8542298c0b73ec5721caa3cc66e1
bhanucheryala9 <cheryalabhanu12@gmail.com>
2023-04-04 10:52:48 -0400

    updated pic of cart added items

--- client/src/components/customer/cart/CartItem.tsx
+++ client/src/components/customer/cart/CartItem.tsx
@@ -12,48 +12,54 @@ import React, { useState } from "react";
 import { faker } from "@faker-js/faker";
 import { OrderInfo } from "../orders/OrderItem";
 
-interface CartItemProps{
+interface CartItemProps {
   item: OrderInfo;
-  setCartInfo: React.Dispatch<React.SetStateAction<OrderInfo[]>>
-  cartInfo: OrderInfo[]
+  setCartInfo: React.Dispatch<React.SetStateAction<OrderInfo[]>>;
+  cartInfo: OrderInfo[];
 }
 
 const CartItem = (props: CartItemProps) => {
-  const { item, setCartInfo, cartInfo} = props;
+  const { item, setCartInfo, cartInfo } = props;
 
-  const prepareData = (option: string) =>{
+  const prepareData = (option: string) => {
     let updatedData;
-    if(option==='add'){
-       updatedData = cartInfo?.map((product)=> {
-        if (product.productName === item.productName){
-          return {...product, quantity: item.quantity+1}
-        }else{
-          return product
+    if (option === "add") {
+      updatedData = cartInfo?.map((product) => {
+        if (product.productName === item.productName) {
+          return { ...product, quantity: item.quantity + 1 };
+        } else {
+          return product;
         }
-      })
+      });
 
-      console.log("************ after adding", updatedData)
-    }else{
-      updatedData = cartInfo?.map((product)=> {
-        if (product.productName === item.productName){
-          return {...product, quantity: item.quantity-1}
-        }else{
-          return product
+      console.log("************ after adding", updatedData);
+    } else {
+      updatedData = cartInfo?.map((product) => {
+        if (product.productName === item.productName) {
+          return { ...product, quantity: item.quantity - 1 };
+        } else {
+          return product;
         }
-    })}
+      });
+    }
     setCartInfo(updatedData);
-    
-  }
+  };
 
+  console.log("*************** image ddata", item);
   return (
     <Flex my="4" key={item.id}>
       <Flex>
-        <Image src={faker.image.food()} maxH={14} maxW={14} borderRadius="lg" />
+        <Image
+          src={item?.url !== "" ? item?.url : faker.image.food()}
+          maxH={14}
+          maxW={14}
+          borderRadius="lg"
+        />
       </Flex>
       <Flex mx="4" direction={"column"} width="100%">
         <HStack justifyContent={"space-between"}>
           <Text fontSize={"lg"} fontWeight="semibold">
-           {item.productName}
+            {item.productName}
           </Text>
           <Text fontSize={"lg"} textColor="orange.600" fontWeight={"semibold"}>
             ${item.price}
@@ -67,9 +73,17 @@ const CartItem = (props: CartItemProps) => {
               colorScheme={"orange"}
               borderColor="orange.500"
             >
-              <InputLeftAddon children="-" onClick={()=>prepareData("sub")}/>
-              <Input type={"number"} defaultValue={item.quantity} value={item.quantity} w={10} />
-              <InputRightAddon children="+" onClick={()=>prepareData("add")} />
+              <InputLeftAddon children="-" onClick={() => prepareData("sub")} />
+              <Input
+                type={"number"}
+                defaultValue={item.quantity}
+                value={item.quantity}
+                w={10}
+              />
+              <InputRightAddon
+                children="+"
+                onClick={() => prepareData("add")}
+              />
             </InputGroup>
           </Text>
         </HStack>
