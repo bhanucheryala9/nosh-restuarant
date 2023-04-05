c585c9d63d0670d5fb08576977dbcd285d72ea29
Sreeya Meela <sreeyameela@Sreeyas-MacBook-Air.local>
2023-04-05 11:30:00 -0400

    current step status added

--- client/src/components/customer/tracking/Tracking.tsx
+++ client/src/components/customer/tracking/Tracking.tsx
@@ -1,15 +1,24 @@
 import { Button, Flex, HStack, Text, VStack, Image } from "@chakra-ui/react";
+import { Divider, Steps } from "antd";
 import axios from "axios";
 import React, { createRef, useCallback, useEffect, useState } from "react";
 import orderPlaced from "../../..//assets/order-placed.jpg";
 import accepted from "../../..//assets/accepted.jpg";
 import preparing from "../../..//assets/preparing.jpg";
 import ready from "../../..//assets/ready.jpg";
+import { ImCancelCircle } from "react-icons/im";
+import { useNavigate } from "react-router-dom";
 const Tracking = () => {
-
   const [trackorder, setTrackOrder] = useState();
   const [stepDetails, setStepDetails] = useState(0);
 
+  const statuses = ["processing", "preparing", "ready"];
+  const navigate = useNavigate();
+
+  const getCurrentStep = (status: string) => {
+    const num = statuses.indexOf(status);
+    return num + 1;
+  };
 
 
   const statusProps = [
