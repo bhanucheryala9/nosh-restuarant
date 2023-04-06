c0c74cc1fe55933ac318020182cf89e050c5a0c1
Sreeya Meela <sreeyameela@Sreeyas-MacBook-Air.local>
2023-04-06 11:30:00 -0400

    trying sample bot

--- client/src/components/common/utils.ts
+++ client/src/components/common/utils.ts
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './App.css';
+import ChatBot from 'react-simple-chatbot';
+const steps = [
+  {
+    id: '0',
+    message: 'Welcome Order & Enjoy the food in NOSH! Is this your first order?',
+    trigger: '1',
+  },
+  {
+    id: '1',
+    message: 'Is this your first order',
+    trigger: '2',
+  },
+  
+];
 
-export default function utils() {
+const config = {
+  floating: true,
+};
+
+const App = () => {
+
+  
+
   return (
-    <div>
-      
+    <div className="App">
+      <ThemeProvider theme={theme}>
+        <ChatBot
+          headerTitle="Nosh-Bot"
+          steps={steps}
+          {...config}
+        />
+      </ThemeProvider>
     </div>
-  )
+  );
 }
+
+export default App;
