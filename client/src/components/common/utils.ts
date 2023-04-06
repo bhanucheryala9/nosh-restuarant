f139681cbf24b7589d39d14183548bdb4f66ba57
Sreeya Meela <sreeyameela@Sreeyas-MacBook-Air.local>
2023-04-06 11:30:00 -0400

    styling bot

--- client/src/components/common/utils.ts
+++ client/src/components/common/utils.ts
@@ -1,6 +1,21 @@
 import React, { useState } from 'react';
 import './App.css';
 import ChatBot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
+import botAvatar from './pics/nosh.png';
+
+const theme = {
+  background: 'white',
+  headerBgColor: '#ed872d',
+  headerFontSize: '20px',
+  botBubbleColor: '#F5F5F5',
+  headerFontColor: 'white',
+  botFontColor: 'black',
+  userBubbleColor: '#ed872d',
+  userFontColor: 'white',
+  headerImage: 'none',
+};
+
 const steps = [
   {
     id: '0',
@@ -30,6 +45,11 @@ const App = () => {
           headerTitle="Nosh-Bot"
           steps={steps}
           {...config}
+          botAvatar={botAvatar}
+          headerAvatar={botAvatar}
+          handleEnd={handleEnd}
+          handleReset={handleReset}
+
         />
       </ThemeProvider>
     </div>
