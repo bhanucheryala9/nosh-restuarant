9ae3bc8af909c59e47eddc26a49fb40522b1c58c
sriharshadhanenkula <sriharshadhanenkula@gmail.com>
2023-04-10 13:21:00 +0530

    triggers added to Chatbot

--- client/src/components/common/utils.ts
+++ client/src/components/common/utils.ts
@@ -32,3 +32,109 @@ export const getStatusColors = (status: string | boolean) => {
     return "green";
   }
 };
+
+abel: "Lamb Biryani", trigger: "8" },
+      { value: "Fish Biryani", label: "Fish Biryani", trigger: "8" },
+      { value: "Goat Biryani", label: "Goat Biryani", trigger: "8" },
+      {
+        value: "Vegetable Dum Biryani",
+        label: "Vegetable Dum Biryani",
+        trigger: "8",
+      },
+    ],
+  },
+  {
+    id: "19",
+    message: "Which Main Course do you want to order?",
+    trigger: "24",
+  },
+  {
+    id: "24",
+    options: [
+      { value: "Kadai Chicken", label: "Kadai Chicken", trigger: "8" },
+      { value: "Butter Chicken", label: "Butter Chicken", trigger: "8" },
+      { value: "Egg Masala", label: "Egg Masala", trigger: "8" },
+      { value: "Goat Curry", label: "Goat Curry", trigger: "8" },
+      { value: "Shrimp Masala", label: "Shrimp Masala", trigger: "8" },
+      { value: "Palak Paneer", label: "Palak Paneer", trigger: "8" },
+      { value: "Mixed Veg Masala", label: "Mixed Veg Masala", trigger: "8" },
+    ],
+  },
+  {
+    id: "20",
+    message: "Which Indo Chineese dish do you want to order?",
+    trigger: "25",
+  },
+  {
+    id: "25",
+    options: [
+      { value: "Fried Rice", label: "Fried Rice", trigger: "8" },
+      { value: "Hakka Noodles", label: "Hakka Noodles", trigger: "8" },
+      { value: "Chicken Manchuria", label: "Chicken Manchuria", trigger: "8" },
+    ],
+  },
+  {
+    id: "26",
+    message: "Which Beverage do you want to order?",
+    trigger: "27",
+  },
+  {
+    id: "27",
+    options: [
+      { value: "Tea", label: "Tea", trigger: "8" },
+      { value: "Coffee", label: "Coffee", trigger: "8" },
+      { value: "Butter Milk", label: "Butter Milk", trigger: "8" },
+      { value: "Mango Shake", label: "Mango Shake", trigger: "8" },
+    ],
+  },
+  {
+    id: "4",
+    message: "May I know your email?",
+    trigger: "5",
+  },
+  {
+    id: "5",
+    user: true,
+    metadata: {
+      email: "email",
+      value: "{{{raw}}}",
+    },
+    trigger: "6",
+    validator: (value: any) => {
+      if (validator.validate(value)) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+  },
+  {
+    id: "6",
+    message: "Would you like to order your previous-order?",
+    trigger: "7",
+  },
+  {
+    id: "7",
+    options: [
+      { value: "Yes", label: "Yes", trigger: "8" },
+      { value: "No", label: "No", trigger: "14" },
+    ],
+  },
+  {
+    id: "8",
+    message: "Are you sure? Do you want to order?",
+    trigger: "10",
+  },
+  {
+    id: "10",
+    options: [
+      { value: "Yes", label: "Yes", trigger: "11" },
+      { value: "No", label: "No", trigger: "14" },
+    ],
+  },
+  {
+    id: "11",
+    message: "Your order is added to cart. Please proceed for the payment",
+    end: true,
+  },
+];
