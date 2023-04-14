f5f5ed9049304a4233bbcea0e47264b89e9dc543
Akhil <apabbathireddy@albany.edu>
2023-04-14 20:22:00 +0530

    final changes

--- client/src/components/admin/rewards/CreateReward.tsx
+++ client/src/components/admin/rewards/CreateReward.tsx
@@ -213,132 +213,179 @@ import {
                         textColor="gray.600"
                         fontWeight={"semibold"}
                       >
-                         Discount Percentage
-                    </FormLabel>
-                    <Input
-                      type="number"
-                      defaultValue={defaultData?.discountPercentage}
-                      placeholder="Enter discount"
-                      {...register("discountPercentage", {
-                        required: "Discount Percentage is required",
-                      })}
-                      onChange={(e) => {
-                        const data = {
-                          ...formData,
-                          discountPercentage: e.target.value,
-                        };
-                        setFormData(data as any);
-                      }}
-                    />
-                    <FormErrorMessage>
-                      {errors["discountPercentage"]?.message as string}
-                    </FormErrorMessage>
-                  </FormControl>
-                </GridItem>
-
-                <GridItem rowSpan={1} colSpan={1}>
-                  <FormControl isInvalid={!!errors["maxDiscountAmount"]}>
-                    <FormLabel
-                      fontSize={"xs"}
-                      textColor="gray.600"
-                      fontWeight={"semibold"}
-                    >
-                      Max Discount Amount
-                    </FormLabel>
-                    <Input
-                      type="number"
-                      defaultValue={defaultData?.maxDiscountAmount}
-                      placeholder="Max Discount Amount"
-                      {...register("maxDiscountAmount", {
-                        required: "Max Disount is required",
-                      })}
-                      onChange={(e) => {
-                        const data = {
-                          ...formData,
-                          maxDiscountAmount: e.target.value,
-                        };
-                        setFormData(data as any);
-                      }}
-                    />
-                    <FormErrorMessage>
-                      {errors["maxDiscountAmount"]?.message as string}
-                    </FormErrorMessage>
-                  </FormControl>
-                </GridItem>
-                <GridItem rowSpan={1} colSpan={2}>
-                  <FormControl isInvalid={!!errors["minOrderPrice"]}>
-                    <FormLabel
-                      fontSize={"xs"}
-                      textColor="gray.600"
-                      fontWeight={"semibold"}
-                    >
-                      Min Order Price
-                    </FormLabel>
-                    <InputGroup>
-                      <InputLeftElement
-                        pointerEvents="none"
-                        color="gray.300"
-                        fontSize="1.2em"
-                        children="$"
-                      />
+                        Discount Percentage
+                      </FormLabel>
                       <Input
                         type="number"
-                        defaultValue={defaultData?.minOrderPrice}
-                        placeholder="Min Order Price"
-                        {...register("minOrderPrice", {
-                          required: "Min Order Price is required",
+                        defaultValue={defaultData?.discountPercentage}
+                        placeholder="Enter discount"
+                        {...register("discountPercentage", {
+                          required: "Discount Percentage is required",
                         })}
                         onChange={(e) => {
                           const data = {
                             ...formData,
-                            minOrderPrice: e.target.value,
+                            discountPercentage: e.target.value,
                           };
                           setFormData(data as any);
                         }}
                       />
-                    </InputGroup>
-                    <FormErrorMessage>
-                      {errors["minOrderPrice"]?.message as string}
-                    </FormErrorMessage>
-                  </FormControl>
-                </GridItem>
-                <GridItem rowSpan={1} colSpan={2}>
-                  <FormControl isInvalid={!!errors["appliesTo"]}>
-                    <FormLabel
-                      fontSize={"xs"}
-                      textColor="gray.600"
-                      fontWeight={"semibold"}
-                    >
-                      Reward Applies To:
-                    </FormLabel>
-                    <Select
-                      placeholder="Reward Applies"
-                      defaultValue={defaultData?.appliesTo}
-                      {...register("appliesTo", {
-                        required: "Applies To is required",
-                      })}
-                      onChange={(e) => {
-                        const data = {
-                          ...formData,
-                          appliesTo: e.target.value,
-                        };
-                        setShowField(e.target.value);
-                        setFormData(data as any);
-                      }}
-                    >
-                      <option value="all" style={{ padding: "0 10px" }}>
-                        All
-                      </option>
-                      <option value="category" style={{ padding: "0 10px" }}>
-                        Category
-                      </option>
-                    </Select>
-                    <FormErrorMessage>
-                      {errors["appliesTo"]?.message as string}
-                    </FormErrorMessage>
-                  </FormControl>
-                </GridItem>
-                {showField === "category" && (
+                      <FormErrorMessage>
+                        {errors["discountPercentage"]?.message as string}
+                      </FormErrorMessage>
+                    </FormControl>
+                  </GridItem>
+  
+                  <GridItem rowSpan={1} colSpan={1}>
+                    <FormControl isInvalid={!!errors["maxDiscountAmount"]}>
+                      <FormLabel
+                        fontSize={"xs"}
+                        textColor="gray.600"
+                        fontWeight={"semibold"}
+                      >
+                        Max Discount Amount
+                      </FormLabel>
+                      <Input
+                        type="number"
+                        defaultValue={defaultData?.maxDiscountAmount}
+                        placeholder="Max Discount Amount"
+                        {...register("maxDiscountAmount", {
+                          required: "Max Disount is required",
+                        })}
+                        onChange={(e) => {
+                          const data = {
+                            ...formData,
+                            maxDiscountAmount: e.target.value,
+                          };
+                          setFormData(data as any);
+                        }}
+                      />
+                      <FormErrorMessage>
+                        {errors["maxDiscountAmount"]?.message as string}
+                      </FormErrorMessage>
+                    </FormControl>
+                  </GridItem>
+                  <GridItem rowSpan={1} colSpan={2}>
+                    <FormControl isInvalid={!!errors["minOrderPrice"]}>
+                      <FormLabel
+                        fontSize={"xs"}
+                        textColor="gray.600"
+                        fontWeight={"semibold"}
+                      >
+                        Min Order Price
+                      </FormLabel>
+                      <InputGroup>
+                        <InputLeftElement
+                          pointerEvents="none"
+                          color="gray.300"
+                          fontSize="1.2em"
+                          children="$"
+                        />
+                        <Input
+                          type="number"
+                          defaultValue={defaultData?.minOrderPrice}
+                          placeholder="Min Order Price"
+                          {...register("minOrderPrice", {
+                            required: "Min Order Price is required",
+                          })}
+                          onChange={(e) => {
+                            const data = {
+                              ...formData,
+                              minOrderPrice: e.target.value,
+                            };
+                            setFormData(data as any);
+                          }}
+                        />
+                      </InputGroup>
+                      <FormErrorMessage>
+                        {errors["minOrderPrice"]?.message as string}
+                      </FormErrorMessage>
+                    </FormControl>
+                  </GridItem>
+                  <GridItem rowSpan={1} colSpan={2}>
+                    <FormControl isInvalid={!!errors["appliesTo"]}>
+                      <FormLabel
+                        fontSize={"xs"}
+                        textColor="gray.600"
+                        fontWeight={"semibold"}
+                      >
+                        Reward Applies To:
+                      </FormLabel>
+                      <Select
+                        placeholder="Reward Applies"
+                        defaultValue={defaultData?.appliesTo}
+                        {...register("appliesTo", {
+                          required: "Applies To is required",
+                        })}
+                        onChange={(e) => {
+                          const data = {
+                            ...formData,
+                            appliesTo: e.target.value,
+                          };
+                          setShowField(e.target.value);
+                          setFormData(data as any);
+                        }}
+                      >
+                        <option value="all" style={{ padding: "0 10px" }}>
+                          All
+                        </option>
+                        <option value="category" style={{ padding: "0 10px" }}>
+                          Category
+                        </option>
+                      </Select>
+                      <FormErrorMessage>
+                        {errors["appliesTo"]?.message as string}
+                      </FormErrorMessage>
+                    </FormControl>
+                  </GridItem>
+                  {showField === "category" && (
+                    <GridItem rowSpan={1} colSpan={2}>
+                      <FormControl>
+                        <FormLabel
+                          fontSize={"xs"}
+                          textColor="gray.600"
+                          fontWeight={"semibold"}
+                        >
+                          Select Category:
+                        </FormLabel>
+                        <Select
+                          placeholder="Select Category"
+                          defaultValue={defaultData?.appliedCategory}
+                          {...register("appliedCategory")}
+                          onChange={(e) => {
+                            const data = {
+                              ...formData,
+                              appliedCategory: e.target.value,
+                            };
+                            setFormData(data as any);
+                          }}
+                        >
+                          <option
+                            value="appetizers"
+                            style={{ padding: "0 10px" }}
+                          >
+                            Appetizers
+                          </option>
+                          <option value="biryani" style={{ padding: "0 10px" }}>
+                            Biryani
+                          </option>
+                          <option value="soups" style={{ padding: "0 10px" }}>
+                            Soups
+                          </option>
+                          <option value="indo-chinese">Indo Chinese</option>
+                          <option
+                            value="main-course"
+                            style={{ padding: "0 10px" }}
+                          >
+                            Main Course
+                          </option>
+                          <option value="beverages" style={{ padding: "0 10px" }}>
+                            Beverages
+                          </option>
+                        </Select>
+                      </FormControl>
+                    </GridItem>
+                  )}
                   <GridItem rowSpan={1} colSpan={2}>
                     <FormControl>
                       <FormLabel
@@ -348,90 +395,44 @@ import {
                       >
                         Select Category:
                       </FormLabel>
-                      <Select
-                        placeholder="Select Category"
-                        defaultValue={defaultData?.appliedCategory}
-                        {...register("appliedCategory")}
-                        onChange={(e) => {
-                          const data = {
+                      <DateRange
+                        onChange={(item: any) => {
+                          setFormData({
                             ...formData,
-                            appliedCategory: e.target.value,
-                          };
-                          setFormData(data as any);
+                            startTime: new Date(
+                              item?.selection?.startDate
+                            ).getTime(),
+                            endTime: new Date(item?.selection?.endDate).getTime(),
+                          } as any);
+                          setState([item.selection] as any);
                         }}
-                      >
-                        <option
-                          value="appetizers"
-                          style={{ padding: "0 10px" }}
-                        >
-                          Appetizers
-                        </option>
-                        <option value="biryani" style={{ padding: "0 10px" }}>
-                          Biryani
-                        </option>
-                        <option value="soups" style={{ padding: "0 10px" }}>
-                          Soups
-                        </option>
-                        <option value="indo-chinese">Indo Chinese</option>
-                        <option
-                          value="main-course"
-                          style={{ padding: "0 10px" }}
-                        >
-                          Main Course
-                        </option>
-                        <option value="beverages" style={{ padding: "0 10px" }}>
-                          Beverages
-                        </option>
-                      </Select>
+                        showSelectionPreview={true}
+                        moveRangeOnFirstSelection={false}
+                        months={2}
+                        ranges={state}
+                        direction="horizontal"
+                        preventSnapRefocus={true}
+                        calendarFocus="backwards"
+                      />
                     </FormControl>
                   </GridItem>
-                )}
-                <GridItem rowSpan={1} colSpan={2}>
-                  <FormControl>
-                    <FormLabel
-                      fontSize={"xs"}
-                      textColor="gray.600"
-                      fontWeight={"semibold"}
-                    >
-                      Select Category:
-                    </FormLabel>
-                    <DateRange
-                      onChange={(item: any) => {
-                        setFormData({
-                          ...formData,
-                          startTime: new Date(
-                            item?.selection?.startDate
-                          ).getTime(),
-                          endTime: new Date(item?.selection?.endDate).getTime(),
-                        } as any);
-                        setState([item.selection] as any);
-                      }}
-                      showSelectionPreview={true}
-                      moveRangeOnFirstSelection={false}
-                      months={2}
-                      ranges={state}
-                      direction="horizontal"
-                      preventSnapRefocus={true}
-                      calendarFocus="backwards"
-                    />
-                  </FormControl>
-                </GridItem>
-              </Grid>
-            </ModalBody>
-            <Divider />
-            <ModalFooter>
-              <HStack>
-                <Button onClick={() => setIsModalOpen(false)}>Cancel</Button>
-                <Button colorScheme="orange" mr={3} type="submit">
-                  Create Reward
-                </Button>
-              </HStack>
-            </ModalFooter>
-          </form>
-        </ModalContent>
-      </Modal>
-    </React.Fragment>
-  );
-};
-
-export default CreateReward;
+                </Grid>
+              </ModalBody>
+              <Divider />
+              <ModalFooter>
+                <HStack>
+                  <Button onClick={() => setIsModalOpen(false)}>Cancel</Button>
+                  <Button colorScheme="orange" mr={3} type="submit">
+                    Create Reward
+                  </Button>
+                </HStack>
+              </ModalFooter>
+            </form>
+          </ModalContent>
+        </Modal>
+      </React.Fragment>
+    );
+  };
+  
+  export default CreateReward;
+  
--- client/src/components/customer/tracking/Tracking.tsx
+++ client/src/components/customer/tracking/Tracking.tsx
@@ -38,140 +38,140 @@ const Tracking = () => {
     },
     {
       title: (
-        <Flex alignItems="center"></Flex>
-        <Image src={accepted} h="50px" w="50px" />{" "}
-        <Text ml="2" fontWeight="semibold">
-          {" "}
-          Order Accepted
-        </Text>
-      </Flex>
-    ),
-  },
-  {
-    title: (
-      <Flex alignItems="center">
-        <Image src={preparing} h="50px" w="50px" />{" "}
-        <Text ml="2" fontWeight="semibold">
-          {" "}
-          Preparing
-        </Text>
-      </Flex>
-    ),
-  },
-  {
-    title: (
-      <Flex alignItems="center">
-        <Image src={ready} h="50px" w="50px" />{" "}
-        <Text ml="2" fontWeight="semibold">
-          {" "}
-          Ready
-        </Text>
-      </Flex>
-    ),
-  },
-];
+        <Flex alignItems="center">
+          <Image src={accepted} h="50px" w="50px" />{" "}
+          <Text ml="2" fontWeight="semibold">
+            {" "}
+            Order Accepted
+          </Text>
+        </Flex>
+      ),
+    },
+    {
+      title: (
+        <Flex alignItems="center">
+          <Image src={preparing} h="50px" w="50px" />{" "}
+          <Text ml="2" fontWeight="semibold">
+            {" "}
+            Preparing
+          </Text>
+        </Flex>
+      ),
+    },
+    {
+      title: (
+        <Flex alignItems="center">
+          <Image src={ready} h="50px" w="50px" />{" "}
+          <Text ml="2" fontWeight="semibold">
+            {" "}
+            Ready
+          </Text>
+        </Flex>
+      ),
+    },
+  ];
 
-const getData = () => {
-  const orderID = localStorage.getItem("orderID");
-  axios
-    .get("http://34.235.166.147:5000/api/customer/v1/get-order-details-by-id", {
-      params: {
-        id: orderID,
-      },
-    })
-    .then((response) => {
-      setTrackOrder(response.data.orders);
-      const status = response.data?.orders?.orderStatus;
-      setStepDetails(getCurrentStep(status));
-    })
-    .catch((error) => {
-      console.log("************** error", error);
-    });
-};
-console.log("******* canlled", stepDetails)
-
-useEffect(() => {
-  const intervalCall = setInterval(() => {
-    getData();
-  }, 2000);
-  return () => {
-    // clean up
-    clearInterval(intervalCall);
+  const getData = () => {
+    const orderID = localStorage.getItem("orderID");
+    axios
+      .get("http://34.235.166.147:5000/api/customer/v1/get-order-details-by-id", {
+        params: {
+          id: orderID,
+        },
+      })
+      .then((response) => {
+        setTrackOrder(response.data.orders);
+        const status = response.data?.orders?.orderStatus;
+        setStepDetails(getCurrentStep(status));
+      })
+      .catch((error) => {
+        console.log("************** error", error);
+      });
   };
-}, []);
+  console.log("******* canlled", stepDetails)
 
-const onDaskClicked = () =>{
-  const user = JSON.parse(localStorage.getItem("userInfo")||"");
-  if(user.type==="admin"){
-    navigate("/employee")
-  }else if(user.type ==="employee"){
-    navigate("/employee-orders")
-  }else{
-      navigate("/dashboard")
+  useEffect(() => {
+    const intervalCall = setInterval(() => {
+      getData();
+    }, 2000);
+    return () => {
+      // clean up
+      clearInterval(intervalCall);
+    };
+  }, []);
+
+  const onDaskClicked = () =>{
+    const user = JSON.parse(localStorage.getItem("userInfo")||"");
+    if(user.type==="admin"){
+      navigate("/employee")
+    }else if(user.type ==="employee"){
+      navigate("/employee-orders")
+    }else{
+        navigate("/dashboard")
+    }
+
   }
 
-}
-
-return (
-  <React.Fragment>
-    <Flex w="100%" justifyContent="center" alignItems="center">
-      <Flex
-        bg="white"
-        rounded={"2xl"}
-        my="4"
-        mx="6"
-        px="10"
-        py="6"
-        maxW="60%"
-        direction="column"
-        alignItems="center"
-        shadow="md"
-      >
-        <Text textColor="orange.500" fontSize="2xl" fontWeight="semibold">
-          Check Your Status
-        </Text>
-        <Divider />
-        <HStack justifyContent="space-between" w="100%">
-          <Text>
-            Name:{" "}
-            {(trackorder as any)?.firstName +
-              " " +
-              (trackorder as any)?.lastName}
+  return (
+    <React.Fragment>
+      <Flex w="100%" justifyContent="center" alignItems="center">
+        <Flex
+          bg="white"
+          rounded={"2xl"}
+          my="4"
+          mx="6"
+          px="10"
+          py="6"
+          maxW="60%"
+          direction="column"
+          alignItems="center"
+          shadow="md"
+        >
+          <Text textColor="orange.500" fontSize="2xl" fontWeight="semibold">
+            Check Your Status
           </Text>
-          <Text> ORDER ID: #{(trackorder as any)?.orderId}</Text>
-        </HStack>
-        <Flex mt="16" width="100%">
-          {stepDetails !== -1 ? (
-            <Steps
-              style={{ width: "100%" }}
-              progressDot
-              current={stepDetails}
-              direction="horizontal"
-              items={statusProps}
-            />
-          ) : (
-            <Flex w="550px" direction="column" alignItems="center" mt="3">
-              <ImCancelCircle fontSize="40px" color="red" />
-              <Text
-                mt="6"
-                fontSize="lg"
-                textColor="red.500"
-                fontWeight="semibold"
-              >
-                {" "}
-                Order Cancelled
-              </Text>
-              <Text>Please contact restaurant...! </Text>
-            </Flex>
-          )}
+          <Divider />
+          <HStack justifyContent="space-between" w="100%">
+            <Text>
+              Name:{" "}
+              {(trackorder as any)?.firstName +
+                " " +
+                (trackorder as any)?.lastName}
+            </Text>
+            <Text> ORDER ID: #{(trackorder as any)?.orderId}</Text>
+          </HStack>
+          <Flex mt="16" width="100%">
+            {stepDetails !== -1 ? (
+              <Steps
+                style={{ width: "100%" }}
+                progressDot
+                current={stepDetails}
+                direction="horizontal"
+                items={statusProps}
+              />
+            ) : (
+              <Flex w="550px" direction="column" alignItems="center" mt="3">
+                <ImCancelCircle fontSize="40px" color="red" />
+                <Text
+                  mt="6"
+                  fontSize="lg"
+                  textColor="red.500"
+                  fontWeight="semibold"
+                >
+                  {" "}
+                  Order Cancelled
+                </Text>
+                <Text>Please contact restaurant...! </Text>
+              </Flex>
+            )}
+          </Flex>
+          <Button colorScheme="orange" mt="10" mb="6" onClick={onDaskClicked}>
+            Back to Home Page
+          </Button>
         </Flex>
-        <Button colorScheme="orange" mt="10" mb="6" onClick={onDaskClicked}>
-          Back to Home Page
-        </Button>
       </Flex>
-    </Flex>
-  </React.Fragment>
-);
+    </React.Fragment>
+  );
 };
 
 export default Tracking;
