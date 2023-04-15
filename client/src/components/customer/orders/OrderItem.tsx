c734b4f99ec7d6520cb73e87cd1d2b9199cddd66
sailakkineni <lsaishankar53@gmail.com>
2023-04-15 09:45:00 +0530

    Final changes added to OrderItem page

--- client/src/components/customer/orders/OrderItem.tsx
+++ client/src/components/customer/orders/OrderItem.tsx
@@ -1,112 +1,110 @@
 import {
-    Avatar,
-    Button,
-    ButtonGroup,
-    Card,
-    CardBody,
-    CardFooter,
-    Divider,
-    Flex,
-    Heading,
-    Stack,
-    Text,
-    Image,
-  } from "@chakra-ui/react";
-  import React, { useEffect, useState } from "react";
-  import Biryani from "../../../assets/biryani.jpg";
-  import { useCart } from "../../../contexts/CartContext";
-  import CornerRibbon from "react-corner-ribbon";
-  
-  interface OrderItemsProps {
-    id: string;
-    productName: string;
-    description: string;
-    price: number;
-    discount: number;
-    isAvailable: boolean;
-    tax: number;
-    url: string;
-    category: string;
-    createdAt: string | Date;
-  }
-  
-  export interface OrderInfo {
-    id: string;
-    productName: string;
-    category: string;
-    price: number;
-    quantity: number;
-    url: string;
-  }
+  Avatar,
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  Divider,
+  Flex,
+  Heading,
+  Stack,
+  Text,
+  Image,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import Biryani from "../../../assets/biryani.jpg";
+import { useCart } from "../../../contexts/CartContext";
+import CornerRibbon from "react-corner-ribbon";
 
-  const OrderItem = (props: OrderItemsProps) => {
+interface OrderItemsProps {
+  id: string;
+  productName: string;
+  description: string;
+  price: number;
+  discount: number;
+  isAvailable: boolean;
+  tax: number;
+  url: string;
+  category: string;
+  createdAt: string | Date;
+}
 
-    const {
-        id,
-        productName,
-        description,
-        category,
-        price,
-        discount,
-        isAvailable,
-        tax,
-        url,
-      } = props;
-      const [loading, setLoading] = useState(true);
-      const [error, setError] = useState();
-      const [image, setImage] = useState();
-    
-    
-      useEffect(() => {
-        const fetchImage = async () => {
-          try {
-            const response = await import(
-              `../../../assets/orders/${"chickenpakora.jpg"}`
-            ); // change relative path to suit your needs
-            setImage(response.default);
-          } catch (err) {
-          } finally {
-            setLoading(false);
-          }
-        };
-    
-        fetchImage();
-      }, []);
-      const onItemClicked = () => {
-        const cart = JSON.parse(localStorage.getItem("orders") || "");
-        const filterData =
-          cart.length > 0
-            ? cart.findIndex((item: any) => item.productName === productName) !== -1
-              ? cart.map((item: any) =>
-                  item.id === id ? { ...item, quantity: item.quantity + 1 } : item
-                )
-              : [
-                  ...cart,
-                  {
-                    id: id,
-                    productName: productName,
-                    category: category,
-                    price: price,
-                    quantity: 1,
-                    url: url,
-                  },
-                ]
-            : [
-                {
-                  id: id,
-                  productName: productName,
-                  category: category,
-                  price: price,
-                  quantity: 1,
-                  url: url,
-                },
-              ];
-        localStorage.setItem("orders", JSON.stringify(filterData));
-        // setCartData(filterData);
-      };
-return(
+export interface OrderInfo {
+  id: string;
+  productName: string;
+  category: string;
+  price: number;
+  quantity: number;
+  url: string;
+}
+const OrderItem = (props: OrderItemsProps) => {
+  const {
+    id,
+    productName,
+    description,
+    category,
+    price,
+    discount,
+    isAvailable,
+    tax,
+    url,
+  } = props;
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState();
+  const [image, setImage] = useState();
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      try {
+        const response = await import(
+          `../../../assets/orders/${"chickenpakora.jpg"}`
+        ); // change relative path to suit your needs
+        setImage(response.default);
+      } catch (err) {
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchImage();
+  }, []);
+  const { cartData, setCartData } = useCart();
+  const onItemClicked = () => {
+    const cart = JSON.parse(localStorage.getItem("orders") || "");
+    const filterData =
+      cart.length > 0
+        ? cart.findIndex((item: any) => item.productName === productName) !== -1
+          ? cart.map((item: any) =>
+              item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+            )
+          : [
+              ...cart,
+              {
+                id: id,
+                productName: productName,
+                category: category,
+                price: price,
+                quantity: 1,
+                url: url,
+              },
+            ]
+        : [
+            {
+              id: id,
+              productName: productName,
+              category: category,
+              price: price,
+              quantity: 1,
+              url: url,
+            },
+          ];
+    localStorage.setItem("orders", JSON.stringify(filterData));
+    setCartData(filterData);
+  };
+  return (
     <Card maxW={"72"} key={id}>
-        {!isAvailable && (
+      {!isAvailable && (
         <CornerRibbon
           position="top-right" // OPTIONAL, default as "top-right"
           fontColor="#fff" // OPTIONAL, default as "#f0f0f0"
@@ -119,7 +117,7 @@ return(
         </CornerRibbon>
       )}
       <CardBody>
-      <Image
+        <Image
           filter="auto"
           brightness="70%"
           maxHeight={"56"}
@@ -148,9 +146,8 @@ return(
           Add to cart
         </Button>
       </CardFooter>
-        
     </Card>
-);
-  };
+  );
+};
 
-  export default OrderItem; 
+export default OrderItem;
