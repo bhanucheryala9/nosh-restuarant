ae8210384befaba65571c426b43585d2c1eee87c
Cheryala Bhanu <Cheryala.Bhanu@experian.com>
2023-04-19 22:54:16 -0400

    added items

--- server/mail-service.js
+++ server/mail-service.js
@@ -1,6 +1,29 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
-exports.sendEmail = async (payload) => {
+const fs = require('fs')
+const handlebars = require("handlebars");
+const accountCreateHtml = fs.readFileSync(
+  "./accountCreation.handlebars",
+  "utf-8"
+);
+const accountCreateTemplate = handlebars.compile(accountCreateHtml);
+
+function GetTemplates(purpose, data) {
+  if (purpose === "add-user") {
+    return paymentTemplate({
+      name: "John Doe",
+      orderItems: [
+        { name: "Pizza", price: "$10.99" },
+        { name: "Salad", price: "$5.99" },
+      ],
+      total: "$16.98",
+    });
+  }else{
+    return accountCreateTemplate()
+  }
+}
+
+exports.sendEmail = async (payload, purpose, data) => {
   const { name, email, message, subject } = payload;
   let mailTransporter = nodemailer.createTransport({
     service: "gmail",
@@ -15,6 +38,7 @@ exports.sendEmail = async (payload) => {
     to: email,
     subject: subject,
     text: message,
+    html: GetTemplates(purpose, data),
   };
 
   mailTransporter.sendMail(mailDetails, (error) => {
--- server/routes/customer-router.js
+++ server/routes/customer-router.js
@@ -2,14 +2,24 @@ var express = require("express");
 var router = express.Router();
 var fs = require("fs");
 const orderService = require("../services/orders-services.js");
-
-
+const MailService = require("../mail-service");
 
 router.post("/v1/place-order", async function (req, res, next) {
   const payload = req.body;
-  console.log("Received at router file:", payload)
+  console.log("Received at router file:", payload);
+  const orders = req.body?.orderDetails;
+  const orderspayload = orders?.map((item) => {
+    return {
+      name: item.productName,
+      price: item.price,
+    };
+  });
+
   try {
     const orders = await orderService.createOrders(payload);
+    if (orders) {
+      MailService.sendEmail(mailBody, "order", orderspayload);
+    }
     res.json({ orders: orders, status: "success" });
   } catch (err) {
     res.status(500).json({ error: err.message });
@@ -17,15 +27,14 @@ router.post("/v1/place-order", async function (req, res, next) {
 });
 
 router.get("/v1/get-purchase-history", async function (req, res, next) {
-    const payload = req.query.id;
-    console.log("query at id or email", payload)
-    try {
-      const orders = await orderService.getUserSpecificOrder(payload);
-      res.json({ orders: orders, status: "success" });
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
-  });
-  
+  const payload = req.query.id;
+  console.log("query at id or email", payload);
+  try {
+    const orders = await orderService.getUserSpecificOrder(payload);
+    res.json({ orders: orders, status: "success" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 module.exports = router;
