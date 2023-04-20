672152d59cc2fbf02c21978796b58d55ed326fd8
Cheryala Bhanu <Cheryala.Bhanu@experian.com>
2023-04-20 01:06:19 -0400

    resolved conflicts

--- client/src/components/customer/payments/Payments.tsx
+++ client/src/components/customer/payments/Payments.tsx
@@ -147,12 +147,6 @@ const Payments = () => {
           alertMessage: "Orders Placed successfully..!",
           showAlert: true,
         });
-<<<<<<< HEAD
-        localStorage.setItem("orders", JSON.stringify([]))
-        // const userinfo = JSON.parse(localStorage.getItem("userInfo") ||"");
-        // if(userinfo?.type==="")
-        navigate("/dashboard");
-=======
         const userinfo = JSON.parse(localStorage.getItem("userInfo") ||"")
         if(userinfo?.type === "employee"){
           navigate("/employee-orders");
@@ -160,7 +154,6 @@ const Payments = () => {
           navigate("/dashboard");
 
         }
->>>>>>> main
       })
       .catch((error) => {
         console.log("************** error", error);
